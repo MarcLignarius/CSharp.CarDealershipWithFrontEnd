4e74583c7059003cb4149cfadfb3071a6a63db71
Justin Kerntz and Marc Davies <justin@>
2019-04-30 14:19:50 -0700

    add styles

---
+++ CarDealership/Controllers/CarsController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using CarDealership.Models;
+using System;
+
+namespace CarDealership.Controllers
+{
+  public class CarsController : Controller
+  {
+
+    [HttpGet("/cars")]
+    public ActionResult Index()
+    {
+      List<Cars> allCars = Cars.GetAll();
+      return View(allCars);
+    }
+
+    [Route("/cars/new")]
+    public ActionResult CreateForm()
+    {
+      return View();
+    }
+
+    [HttpPost("/cars")]
+    public ActionResult Create(string makeModel, string color, int mileage, int price)
+    {
+      Cars myCar = new Cars(makeModel, color, mileage, price);
+      return RedirectToAction("Index");
+
+    }
+  }
+}
--- CarDealership/Controllers/HomeController.cs
+++ CarDealership/Controllers/HomeController.cs
@@ -1,31 +1,15 @@
 using Microsoft.AspNetCore.Mvc;
-using System.Collections.Generic;
-using CarDealership.Models;
-using System;
+
 namespace CarDealership.Controllers
 {
   public class HomeController : Controller
   {
 
-    [HttpGet("/")]
-    public ActionResult Index()
-    {
-      List<Cars> allCars = Cars.GetAll();
-      return View(allCars);
-    }
+  [HttpGet("/")]
+  public ActionResult Index()
+  {
+    return View();
+  }
 
-    [Route("/cars/new")]
-    public ActionResult CreateForm()
-    {
-      return View();
-    }
-
-    [HttpPost("/cars")]
-    public ActionResult Create(string makeModel, string color, int mileage, int price)
-    {
-      Cars myCar = new Cars(makeModel, color, mileage, price);
-      return RedirectToAction("Index");
-
-    }
   }
 }
